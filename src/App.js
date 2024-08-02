f98dc0542e7e284621718ae372dd31e1fcc4fb3d
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-02 13:35:57 +0000

    create user context

--- src/App.js
+++ src/App.js
@@ -5,10 +5,31 @@ import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults'
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 
+export const CurrentUserContext = createContext()
+export const SetCurrentUserContext = createContext()
 
 function App() {
+  const [currentUser, setCurrentUser] = useContext(null)
+
+  const handleMount = async (event) => {
+    try {
+      const {data} = await axios.get('dj-rest-auth/user/')
+      setCurrentUser(data)
+    } catch (err){
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    handleMount()
+  }, [])
+
   return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
@@ -20,6 +41,8 @@ function App() {
         </Switch>
       </Container>
     </div>
+    </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
