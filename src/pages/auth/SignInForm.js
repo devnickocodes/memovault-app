be01312270f270ea1b5e93bf40c0dfdf42fa8a44
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-02 13:37:48 +0000

    set current user upon successful sign in

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
@@ -6,8 +6,11 @@ import btnStyles from "../../styles/Button.module.css";
 import feature_image from "../../assets/feature_image.jpg";
 import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
 import axios from "axios";
+import { SetCurrentUserContext } from "../../App";
 
 const SignInForm = () => {
+
+  const setCurrentUser = useContext(SetCurrentUserContext)
 
     const [signInData, setSignInData] = useState({
         username: '',
@@ -31,7 +34,8 @@ const SignInForm = () => {
         event.preventDefault();
         
         try {
-            await axios.post("/dj-rest-auth/login/", signInData);
+            const {data} = await axios.post("/dj-rest-auth/login/", signInData);
+            setCurrentUser(data.user)
             history.push('/')
         } catch (err) {
             setErrors(err.response?.data)
