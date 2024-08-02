3b049b1cff874291ed597cf0691ae3d9871e2898
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-02 10:17:11 +0000

    install axios and add axiosDefaults

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
+import './api/axiosDefaults'
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
           <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
-          <Route exact render={() => <h1>Page Not Found</h1>} />
+          <Route render={() => <h1>Page Not Found</h1>} />
         </Switch>
       </Container>
     </div>
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://memovault-api-2cb24ac3e7ac.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
