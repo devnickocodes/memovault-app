e433a75260490fe682ae29745b810af64b6d91dc
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-02 11:37:45 +0000

    add sign up form and style input fields

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -4,15 +4,39 @@ import { Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import feature_image from '../../assets/feature_image.jpg'
+import feature_image from "../../assets/feature_image.jpg";
 import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
-        <Container className= "p-4">
-          <h1 className={styles.Header}>Sign <span>up</span></h1>
+        <Container className="p-4">
+          <h1 className={`mb-5 ${styles.Header}`}>
+            Sign <span>up</span>
+          </h1>
+
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control className={styles.Input} type="text" placeholder="Username" name="username" />
+            </Form.Group>
+
+            <Form.Group controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control className={styles.Input} type="password" placeholder="Password" name="password1"/>
+            </Form.Group>
+
+            <Form.Group controlId="password2">
+              <Form.Label className="d-none">Confirm Password</Form.Label>
+              <Form.Control className={styles.Input} type="password" placeholder="Confirm Password" name="password2"/>
+            </Form.Group>
+
+            <Button type="submit">
+              Sign Up
+            </Button>
+          </Form>
+
         </Container>
         <Container className={`mt-3 ${styles.Content}`}>
           <Link className={styles.SignInLink} to="/signin">
@@ -20,14 +44,8 @@ const SignUpForm = () => {
           </Link>
         </Container>
       </Col>
-      <Col
-        md={6}
-        className="my-auto d-none d-md-block p-2"
-      >
-        <Image
-          className={`${styles.FeatureImage}`}
-          src={feature_image}
-        />
+      <Col md={6} className="my-auto d-none d-md-block p-2">
+        <Image className={`${styles.FeatureImage}`} src={feature_image} />
       </Col>
     </Row>
   );
