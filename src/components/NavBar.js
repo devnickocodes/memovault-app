d4281755d4183acdfa72438187af86afd47dd255
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-03 07:08:56 +0000

    add navbar link for creation of posts

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,19 @@ import { useCurrentUser } from "../contexts/CurrentUserContext";
 const NavBar = () => {
   const currentUser = useCurrentUser()
 
-  const loggedInIcons = <>{currentUser?.username}</>
+  const addPostIcon = (
+    <NavLink
+    className={styles.NavLink}
+    activeClassName={styles.Active}
+    to="/posts/create"
+  >
+    <i className="fa-solid fa-circle-plus mr-2"></i>Add Post
+  </NavLink>
+  )
+
+  const loggedInIcons = <>
+  
+  </>
 
   const loggedOutIcons = (
     <>
@@ -37,6 +49,7 @@ const NavBar = () => {
             <i className="fa-solid fa-icons ml-3"></i>
           </Navbar.Brand>
         </NavLink>
+        {currentUser && addPostIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
