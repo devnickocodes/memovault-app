657bb3b4d101c9c76231f9ce8f6bbf0f34b88f49
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-05 10:48:33 +0000

    add handleSubmit handler for the post create form

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
@@ -12,6 +12,8 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css"
 import Asset from "../../components/Asset";
 import { Image } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostCreateForm() {
   const [errors, setErrors] = useState({});
@@ -24,6 +26,10 @@ function PostCreateForm() {
 
   const {title, content, image} = postData
 
+  const inputImage = useRef(null)
+
+  const history = useHistory()
+
   const handleChange = (event) => {
     setPostData({
         ...postData,
@@ -31,6 +37,23 @@ function PostCreateForm() {
     })
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const formData = new FormData()
+
+    formData.append('title', title)
+    formData.append('content', content)
+    formData.append('image', inputImage.current.files[0])
+
+    try {
+        const {data} = await axiosReq.post('/posts/', formData)
+        history.push(`/posts/${data.id}`)
+    } catch (err) {
+        if (err.response?.status !== 401){
+            setErrors(err.response?.data)
+        }
+    }
+  }
 
   const handleChangeImage = (event) => {
     if  (event.target.files.length){
@@ -45,7 +68,7 @@ function PostCreateForm() {
 
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Container
         className={`${styles.Container} d-flex flex-column align-items-center`}
       >
@@ -77,7 +100,7 @@ function PostCreateForm() {
               </Form.Label>
                 )}
               
-              <Form.File className={styles.hiddenFileInput} id="image-upload" accept="image/*" onChange={handleChangeImage}/>
+              <Form.File className={styles.hiddenFileInput} id="image-upload" accept="image/*" onChange={handleChangeImage} ref={inputImage}/>
             </Form.Group>
           </Col>
         </Row>
