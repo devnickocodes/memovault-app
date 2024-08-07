d47bec8d815874d9b0824d35e709dfd622e73882
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-07 13:31:27 +0000

    map over comments and display them

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -24,10 +24,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async (event) => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}/`),
+          axiosReq.get(`/comments/?post=${id}`)
         ]);
         setPost({ results: [post] });
+        setComments(comments);
       } catch {
         setErrors("Sorry, an error occured. Please try again.");
       }
@@ -68,6 +70,15 @@ function PostPage() {
           ) : comments.results.length ? (
             "Comments"
           ) : null}
+          {comments.results.length ? (
+            comments.results.map(comment => (
+              <p key={comment.id}>{comment.owner} : {comment.content}</p>
+            ))
+          ) : currentUser ? (
+            <span>It would be cool if you were the first one to comment!</span>
+          ) : (
+            <span>No comments</span>
+          )}
         </Container>
       </Col>
 
