0e39e11075cd0e314ac78e7d0928af433d024ffa
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-08 10:03:49 +0000

    add like functionality for comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import Media from "react-bootstrap/Media";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
+import postStyles from "../../styles/Post.module.css"
 import { DropdownOptions } from "../../components/DropdownOptions";
 import { axiosRes } from "../../api/axiosDefaults";
-import { Alert } from "react-bootstrap";
+import { Alert, OverlayTrigger, Tooltip } from "react-bootstrap";
 import alertStyles from "../../styles/Post.module.css";
 import CommentEditForm from "./CommentEditForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const Comment = (props) => {
   const {
@@ -21,8 +23,11 @@ const Comment = (props) => {
     id,
     setPost,
     setComments,
+    comment_likes_count,
+    comment_like_id,
   } = props;
 
+  const currentUser = useCurrentUser()
   const [errors, setErrors] = useState(null);
   const [showEditForm, setShowEditForm] = useState(false);
 
@@ -44,6 +49,26 @@ const Comment = (props) => {
       }));
     } catch (err) {
       setErrors("Sorry, there was an error trying to delete the comment. Please try again.");
+    }
+  };
+
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/like/comment/", { comment: id });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) =>
+          comment.id === id
+            ? {
+                ...comment,
+                comment_likes_count: comment.comment_likes_count + 1,
+                comment_like_id: data.id,
+              }
+            : comment
+        ),
+      }));
+    } catch {
+      setErrors("Failed to like the comment. Please try again.");
     }
   };
 
@@ -75,6 +100,32 @@ const Comment = (props) => {
           ) : (
             <p className={styles.CommentContent}>{content}</p>
           )}
+          <div>
+          {is_owner ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>You can't like your own comment!</Tooltip>}
+              >
+                <i className={`fa-regular fa-heart mr-1 ${postStyles.Heart}`}></i>
+              </OverlayTrigger>
+            ) : comment_like_id ? (
+              <span onClick={() => {}}>
+                <i className={`fa-solid fa-heart ${postStyles.Heart}`}></i>
+              </span>
+            ) : currentUser ? (
+              <span onClick={handleLike}>
+                <i className={`fa-regular fa-heart mr-1 ${postStyles.Heart}`}></i>
+              </span>
+            ) : (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Please log in to like comments!</Tooltip>}
+              >
+                <i className={`fa-regular fa-heart mr-1 ${postStyles.Heart}`}></i>
+              </OverlayTrigger>
+            )}
+            {comment_likes_count}
+          </div>
         </Media.Body>
         {(is_owner || is_admin) && !showEditForm && (
           <DropdownOptions
