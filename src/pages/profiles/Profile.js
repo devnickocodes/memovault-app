3224fae3fa6c483f831e54a117e43834b6da2505
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-08 13:24:39 +0000

    add follow/unfollow buttons

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -2,11 +2,14 @@ import React from 'react'
 import styles from "../../styles/Profile.module.css"
 import { Link } from 'react-router-dom'
 import Avatar from "../../components/Avatar"
+import { useCurrentUser } from '../../contexts/CurrentUserContext'
+import { Button } from 'react-bootstrap'
 
 const Profile = (props) => {
 
     const {profile, mobile} = props
     const {id, following_id, image, owner, is_owner} = profile
+    const currentUser = useCurrentUser()
     
   return (
     <div className={`d-flex align-items-center ${mobile && 'flex-column'} ${!mobile && 'my-3'}`}>
@@ -17,6 +20,19 @@ const Profile = (props) => {
         </div>
         <div>
             <p>{owner}</p>
+        </div>
+        <div className={`text-right ${!mobile && 'ml-auto'}`}>
+            {!mobile && currentUser && !is_owner && (
+                following_id ? (
+                    <Button
+                    onClick={() => {}}
+                    >Unfollow</Button>
+                ) : (
+                    <Button
+                    onClick={() => {}}
+                    >Follow</Button>
+                )
+            )}
         </div>
     </div>
   )
