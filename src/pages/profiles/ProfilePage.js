2872a1730767558d1303ddeb9d105dc324239a55
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-12 11:26:28 +0000

    add some styling and adjust structure of profile card

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -3,62 +3,80 @@ import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
 import Asset from "../../components/Asset";
-
 import PopularProfilesMostPosts from "./PopularProfilesMostPosts";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
-import { useProfileData, useSetProfileData } from "../../contexts/ProfileDataContext";
-import { Image } from "react-bootstrap";
+import {
+  useProfileData,
+  useSetProfileData,
+} from "../../contexts/ProfileDataContext";
+import { Button, Card, Image } from "react-bootstrap";
+import btnStyles from "../../styles/Button.module.css";
+import styles from "../../styles/Button.module.css";
+
+
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
-  const currentUser = useCurrentUser();
 
-  const {id} = useParams()
-  const setProfileData = useSetProfileData()
-  const {profileData} = useProfileData()
-  const pageProfile = profileData?.pageProfile
-  const profile = pageProfile?.results[0]
-
+  const { id } = useParams();
+  const setProfileData = useSetProfileData();
+  const { profileData } = useProfileData();
+  const pageProfile = profileData?.pageProfile;
+  const profile = pageProfile?.results[0];
 
   useEffect(() => {
     const fetchData = async () => {
-        try {
-            const { data: profileData } = await axiosReq.get(`/profiles/${id}/`);
-            setProfileData((prevState) => ({
-                ...prevState,
-                pageProfile: {results: [profileData]}
-            }))
-            setHasLoaded(true);
-        } catch(err) {
-            console.log(err)
-        }
-    }
-    fetchData()
-  }, [id, setProfileData])
+      try {
+        const { data: profileData } = await axiosReq.get(`/profiles/${id}/`);
+        setProfileData((prevState) => ({
+          ...prevState,
+          pageProfile: { results: [profileData] },
+        }));
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [id, setProfileData]);
 
   const mainProfile = (
-    <>
-      <Row noGutters className="px-3 text-center">
-        <Col lg={3} className="text-lg-left">
-          <Image src={profile?.image} width={100} />
-        </Col>
-        <Col lg={6}>
-          <p className="m-2">{profile?.owner}</p>
-          <p className="m-2">{profile?.name}</p>
-          <p className="m-2">{profile?.bio}</p>
-          <p className="m-2">{profile?.hobbies}</p>
-          <p>Profile stats</p>
-        </Col>
-        <Col lg={3} className="text-lg-right">
-        <p>Follow button</p>
-        </Col>
-        <Col className="p-3">Profile content</Col>
-      </Row>
-    </>
+    <Card className={styles.ProfileCard}>
+      <Card.Body>
+        <Row>
+          <Col lg={4} className="text-center">
+            <Image
+              src={profile?.image}
+              alt={`${profile?.owner}'s profile image`}
+              className={styles.ProfileImage}
+              roundedCircle
+              width={175}
+              height={175}
+            />
+          </Col>
+          <Col className="text-center" lg={8}>
+            <Card.Title className="mb-2">{profile?.owner}</Card.Title>
+            <Card.Subtitle className="mb-2">
+              {profile?.name} Placeholder Name
+            </Card.Subtitle>
+            <Card.Text>
+              {profile?.follows_you ? "Follows you" : "Does not follow you"}
+            </Card.Text>
+            {profile?.hobbies ? (
+              <Card.Text>{profile?.hobbies}</Card.Text>
+            ) : (
+              <p>No Hobbies</p>
+            )}
+            <Button className={btnStyles.Button}>Follow</Button>
+          </Col>
+        </Row>
+        <Row className="mt-4 text-center">
+          <Col>{profile?.bio ? <p>{profile?.bio}</p> : <p>No Bio</p>}</Col>
+        </Row>
+      </Card.Body>
+    </Card>
   );
 
   const mainProfilePosts = (
