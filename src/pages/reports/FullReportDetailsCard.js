448814d9730eac8e9dc2679aa5024d54a704d708
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-19 08:28:20 +0000

    style full report card

--- src/pages/reports/FullReportDetailsCard.js
+++ src/pages/reports/FullReportDetailsCard.js
@@ -3,6 +3,9 @@ import { Link, useParams } from 'react-router-dom/cjs/react-router-dom.min'
 import { axiosReq } from '../../api/axiosDefaults';
 import Asset from '../../components/Asset';
 import { Alert, Button, Card } from 'react-bootstrap';
+import navStyles from "../../styles/NavBar.module.css"
+import styles from "../../styles/FullReportDetailsCard.module.css"
+import btnStyles from "../../styles/Button.module.css"
 
 const FullReportDetailsCard = () => {
 
@@ -27,7 +30,7 @@ const FullReportDetailsCard = () => {
 
 
     return (
-        <Card>
+        <Card className={styles.Card}>
             <Card.Body>
                 {!loaded ? (
                     <Asset spinner />
@@ -35,9 +38,19 @@ const FullReportDetailsCard = () => {
                     <Alert>{error}</Alert>
                 ) : (
                     <>
-                        <p>{report.post.title}</p>
-                        <Link to={`/posts/${report.post.id}`}>POST</Link>
-                        
+                       <Card.Title className={`${styles.cardTitle} ${navStyles.Logo} ${styles.Bold} mb-4`}>Full Report <span>Details</span></Card.Title>
+                       <Card.Subtitle className={`${styles.cardSubtitle} mb-3`}>Reason for report: </Card.Subtitle>
+                       <Card.Text className={styles.cardText}>{report?.reason}</Card.Text>
+                       <Card.Subtitle className={`${styles.cardSubtitle} mb-3`}>Additional Reason: </Card.Subtitle>
+                       <Card.Text className={styles.cardText}>{report?.custom_reason}</Card.Text>
+                       <Card.Subtitle className={`${styles.cardSubtitle} mb-3`}>Reported Post Title: </Card.Subtitle>
+                       <Card.Text className={styles.cardText}>{report?.post?.title}</Card.Text>
+                       <Card.Subtitle className={`${styles.cardSubtitle} mb-3`}>Reported Post Content: </Card.Subtitle>
+                       <Card.Text className={styles.cardText}>{report?.post?.content}</Card.Text>
+                       <Card.Subtitle className={`text-muted mb-3`}>Reported Issued On: {report?.updated_at}</Card.Subtitle>
+                       <Link>
+                        <Button className={btnStyles.Button}>Go To Post</Button>
+                       </Link>
                     </>
                 )}
             </Card.Body>
