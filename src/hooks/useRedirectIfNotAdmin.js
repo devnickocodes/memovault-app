08146345425ae15fbf9b4b500e7e7dfc00eec865
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-20 12:28:30 +0000

    adjust hook to check if admin

--- src/hooks/useRedirectIfNotAdmin.js
+++ src/hooks/useRedirectIfNotAdmin.js
@@ -1,18 +1,16 @@
-import { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
-export const useRedirectIfNotAdmin = (apiEndpoint, redirectTo) => {
-    const history = useHistory();
-    const currentUser = useCurrentUser();
+const useRedirectIfNotAdmin = () => {
+  const history = useHistory();
+  const currentUser = useCurrentUser();
 
-    useEffect(() => {
-        const checkIfAdmin = () => {
-            if (apiEndpoint.includes('/admin') && (!currentUser || !currentUser.is_admin)) {
-                history.push(redirectTo);
-            }
-        };
-
-        checkIfAdmin();
-    }, [apiEndpoint, currentUser, history, redirectTo]);
+  useEffect(() => {
+    if (currentUser && !currentUser.is_admin) {
+      history.push('/');
+    }
+  }, [currentUser, history]);
 };
+
+export default useRedirectIfNotAdmin;
