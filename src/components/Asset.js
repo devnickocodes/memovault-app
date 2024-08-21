f4f150157bd87ae33ec20a9e1ab6dbcde7f96ca3
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-21 07:50:00 +0000

    adjust asset component and style not found component

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Spinner } from "react-bootstrap";
 import styles from "../styles/Asset.module.css";
 
-const Asset = ({ spinner, src, message, height, width }) => {
+const Asset = ({ spinner, src, message, height, width, borderRadius }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
       {spinner && <Spinner animation="border" />}
-      {src && <img src={src} alt={message} height={height} width={width} />}
+      {src && <img src={src} alt={message} height={height} width={width} style={{ borderRadius: borderRadius }}/>}
       {message && <p className="mt-4">{message}</p>}
     </div>
   );
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,42 @@
 import React from 'react'
 import NoResults from "../assets/no-results.jpg"
 import Asset from './Asset'
+import PopularProfilesMostPosts from '../pages/profiles/PopularProfilesMostPosts'
+import { Button, Col, Container, Row } from 'react-bootstrap'
+import PopularPosts from '../pages/posts/PopularPosts'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import btnStyles from "../styles/Button.module.css"
 
 const NotFound = () => {
-  return (
-    <Asset src={NoResults} />
-  )
+
+    return (
+        <Row className="h-100">
+          <Col lg={8} className="py-2 p-0 p-lg-2">
+          <PopularProfilesMostPosts mobile />
+            <Container className="mt-3">
+              <Asset message="We are sorry, but the page you were looking for can't be found!" src={NoResults} alt="Not Found" height={200} width={200} borderRadius="10px"/>
+              <div className="text-center">
+                <Link to='/'>
+                    <Button className={btnStyles.Button}>
+                        Go To Home
+                    </Button>
+                </Link>
+              </div>
+            </Container>
+          </Col>
+    
+          <Col lg={4} className="d-none d-lg-flex flex-column p-lg-2">
+            <div className="d-lg-none mb-3">
+              <PopularProfilesMostPosts />
+            </div>
+            <div className="d-none d-lg-flex flex-column">
+              <PopularProfilesMostPosts />
+              <PopularPosts />
+            </div> 
+          </Col>
+        </Row>
+      );
+
 }
 
 export default NotFound
