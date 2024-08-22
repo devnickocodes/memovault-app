7d3ca0f0a9608418cd715087c1fd945a2787d829
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-22 09:17:18 +0000

    adjust styling for report previews

--- src/pages/reports/ReportPreview.js
+++ src/pages/reports/ReportPreview.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import btnStyles from "../../styles/Button.module.css";
-import postStyles from "../../styles/Post.module.css";
+import styles from "../../styles/ReportPreview.module.css";
+import popularProfilesStyles from "../../styles/PopularProfiles.module.css"
+
 import { scrollToTop } from '../../utils/scrollToTop';
 
 const ReportPreview = ({ report, apiEndpoint }) => {
@@ -15,28 +17,46 @@ const ReportPreview = ({ report, apiEndpoint }) => {
   const handleScroll = () => scrollToTop()
 
   return (
-    <Card className={`${postStyles.Container} mb-4`}>
+    <Card className={`${popularProfilesStyles.Container} mb-4`}>
       <Card.Body>
         {report.is_admin && (
-          <Card.Title className={`${postStyles.CardTitle} mb-3`}>
-          Report submitted by: {report?.owner}
-        </Card.Title>
+          <Card.Title className={`${styles.CardTitle} mb-3`}>
+              <span className={styles.Label}>Submitted by: </span>
+              <span>{report?.owner}</span>
+          </Card.Title>
         )}
-        <Card.Title className={`${postStyles.CardTitle} mb-3`}>
-          Reason for report: {report?.reason}
+        
+        <Card.Title className={`${styles.CardTitle} mb-3`}>
+          <span className={styles.Label}>Reason: </span>
+          <span className={styles.Value}>{report?.reason}</span>
         </Card.Title>
-        <Card.Subtitle className="mb-3">
-          <span className={`${postStyles.CardTitle}`}>Additional Reason:</span> 
-          {customReason.length > 10 ? `${customReason.slice(0, 100)}...` : customReason}
+            
+       {customReason && (
+        <Card.Subtitle className={`${styles.CardSubtitle} mb-3`}>
+            <span className={styles.Label}>Additional Reason: </span>
+            <span className={styles.Value}>
+                {customReason.length > 100 ? `${customReason.slice(0, 100)}...` : customReason}
+            </span>
         </Card.Subtitle>
-        <Card.Subtitle className="mb-3">
-          <span className={`${postStyles.CardTitle}`}>Reported Post Title: </span> 
-          {postTitle.length > 100 ? `${postTitle.slice(0, 100)}...` : postTitle}
-        </Card.Subtitle>
-        <Card.Text>
-          <span className={`${postStyles.CardTitle}`}>Reported Post Content: </span> 
-          {postContent.length > 100 ? `${postContent.slice(0, 100)}...` : postContent}
-        </Card.Text>
+       )}
+
+        
+      <Card.Subtitle className={`${styles.CardSubtitle} mb-3`}>
+                    <span className={styles.Label}>Post Title: </span>
+                    <span>
+                        {postTitle.length > 100 ? `${postTitle.slice(0, 100)}...` : postTitle}
+                    </span>
+                </Card.Subtitle>
+
+
+                <Card.Text className="mb-4">
+                    <span className={styles.Label}>Post Content: </span>
+                    <span>
+                        {postContent.length > 100 ? `${postContent.slice(0, 100)}...` : postContent}
+                    </span>
+                </Card.Text>
+
+
         <Link to={`${apiEndpoint}/${report?.id}`}>
           <Button onClick={handleScroll} className={btnStyles.Button}>View Full Report</Button>
         </Link>
