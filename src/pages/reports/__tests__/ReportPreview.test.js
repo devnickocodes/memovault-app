2b548dbae409587b253d58d28be7b305f4431a4b
Nikolay Hristev <devnickocodes@gmail.com>
2024-08-23 09:04:32 +0000

    add a test for report preview and its contents for non admins

--- src/pages/reports/__tests__/ReportPreview.test.js
+++ src/pages/reports/__tests__/ReportPreview.test.js
@@ -14,8 +14,19 @@ const mockReportAdmin = {
   is_admin: true,
 };
 
-test("renders ReportPreview with correct content for admin", () => {
+const mockReportNonAdmin = {
+  id: 2,
+  owner: "Jane Doe",
+  reason: "Inappropriate",
+  custom_reason: "",
+  post: {
+    title: "Post Title",
+    content: "Post Content.",
+  },
+  is_admin: false,
+};
 
+test("renders ReportPreview with correct content for admin", () => {
   const apiEndpoint = "/reports/admin";
 
   render(
@@ -50,6 +61,53 @@ test("renders ReportPreview with correct content for admin", () => {
 
   expect(screen.getByText(/post content:/i)).toBeInTheDocument();
   expect(screen.getByText(mockReportAdmin.post.content)).toBeInTheDocument();
+
+  // Check for the "View Full Report" button
+  expect(
+    screen.getByRole("button", { name: /view full report/i })
+  ).toBeInTheDocument();
+});
+
+test("renders ReportPreview with correct content for non-admin", () => {
+  const apiEndpoint = "/reports";
+
+  render(
+    <Router>
+      <ReportPreview report={mockReportNonAdmin} apiEndpoint={apiEndpoint} />
+    </Router>
+  );
+
+  // Verify that "submitted by" elements are not present if not an admin
+  expect(screen.queryByText(/submitted by:/i)).toBeNull();
+  expect(screen.queryByText(mockReportNonAdmin.owner)).toBeNull();
+
+  // Check that the link is correct for non-admins
+  const link = screen.getByRole("link");
+  expect(link).toHaveAttribute(
+    "href",
+    `${apiEndpoint}/${mockReportNonAdmin.id}`
+  );
+
+  //  Check for "Reason:" label and its text
+  const reasonLabels = screen.getAllByText(/reason:/i);
+  expect(reasonLabels.length).toBeGreaterThan(0);
+  expect(screen.getByText(mockReportNonAdmin.reason)).toBeInTheDocument();
+
+  // Check for the "Additional Reason:" label and its text if custom_reason exists
+  if (mockReportNonAdmin.custom_reason) {
+    const additionalReasonLabels = screen.getAllByText(/additional reason:/i);
+    expect(additionalReasonLabels.length).toBeGreaterThan(0);
+    expect(
+      screen.getByText(mockReportNonAdmin.custom_reason)
+    ).toBeInTheDocument();
+  }
+
+  // Check for post title and content
+  expect(screen.getByText(/post title:/i)).toBeInTheDocument();
+  expect(screen.getByText(mockReportNonAdmin.post.title)).toBeInTheDocument();
+
+  expect(screen.getByText(/post content:/i)).toBeInTheDocument();
+  expect(screen.getByText(mockReportNonAdmin.post.content)).toBeInTheDocument();
 
   // Check for the "View Full Report" button
   expect(
